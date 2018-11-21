1bcc13d310000721ed8a25902802607810c9dfb2
liyi <shliyi@corp.netease.com>
2018-11-21 19:05:00 +0800

    修改help说明

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ commander
   // .command('init [preset_conf]')
   // .alias('i')
   .description('初始化配置文件')
-  .option("-c, --config <config_path>", "配置文件路径")
-  .option("-b, --bucket <bucket_name>", "桶名")
+  .option("-c, --config <config_path>", "nos配置文件的路径")
+  .option("-b, --bucket <bucket_name>", "nos桶名")
   .option("-l, --list [path_pattern]>", "显示桶目录树")
   .option("-u, --upload <file_path>", "上传文件")
-  .option("-e, --env <env>", "环境变量")
+  .option("-e, --env <env>", "设定上传的环境变量，配合配置文件中的‘pathPrefix’使用")
   .action(init)
   .on('--help', function() {
     console.log('\n Waiting for you!!!');
