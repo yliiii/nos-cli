50c12de2c86b99faf97e1a872cf42f0dad0ba1c0
liyi <shliyi@corp.netease.com>
2018-11-22 14:12:36 +0800

    调整构建命令

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 const path = require('path')
 const gulp = require('gulp')
-const clean = require('gulp-clean')
 const header = require('gulp-header')
 const runSequence = require('run-sequence')
 
 const BIN_DIR = path.resolve(__dirname, 'bin')
 
 const tasks = {
-  clean: function() {
-    return gulp.src(BIN_DIR)
-      .pipe(clean({ force: true }))
-  },
   copyFile: function() {
     return gulp.src('./dist/**/*.*')
       .pipe(gulp.dest(BIN_DIR))
